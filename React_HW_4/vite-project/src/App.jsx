a830de257aa063635f1a57f6222d5abf0db18d31
Alexei <alexeikh14@gmail.com>
2025-07-22 13:09:33 +0300

    commit 3

--- React_HW_4/vite-project/src/App.jsx
+++ React_HW_4/vite-project/src/App.jsx
@@ -6,13 +6,20 @@ import FilterForm from './components/FilterForm';
 import Pagination from './components/Pagination';
 
 function App() {
-// Состояния для сортировки
 const [patients, setPatients] = useState(patientsData);
-const [sortKey, setSortKey] = useState(null);
-const [sortDir, setSortDir] = useState(null);
 
-const sortUsers = (() => {
+// Состояния для сортировки
+const [sortKey, setSortKey] = useState("none");
+const [sortDir, setSortDir] = useState("none");
+
+const sortUsers = ((key) => {
   
+  if (sortKey === key) {
+    setSortDir(sortDir === "none" ? "asc" : "desc");
+  } else {
+    setSortKey(key);
+    setSortDir("none");
+  }
 });
 
 //Состояния для пагинации
